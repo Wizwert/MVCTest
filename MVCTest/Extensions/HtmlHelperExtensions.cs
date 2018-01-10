d3412ba90c1fce13349aed4c6fde08b862bfc98a
Will R <william.rusinko@gmail.com>
2018-01-09 19:38:17 -0600

    Chapter 5

---
+++ MVCTest/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace MVCTest.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString HtmlConvertToJson(this HtmlHelper htmlHelper,
+            object model)
+        {
+            var settings = new JsonSerializerSettings
+                           {
+                               ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                               Formatting = Formatting.Indented
+                           };
+
+            return new HtmlString(JsonConvert.SerializeObject(model, settings));
+        }
+    }
+}
---
+++ MVCTest/Models/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MVCTest.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
